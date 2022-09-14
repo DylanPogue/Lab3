03530d3671d14c98517e39a32e0d7c0ce8510347
Dylan Pogue <POGUEDJ@ETSU.EDU>
2022-09-13 21:40:24 -0400

    Program checkpoint

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,2 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Lab3;
+public class Program
+{
+    static List<Person> people = new List<Person>();
+
+    public static void Main(string[] args)
+    {
+        var option = 0;
+        do
+        {
+            DisplayMenu();
+            option = Int32.Parse(Console.ReadLine());
+            MenuChoice(option);
+
+            Console.WriteLine("\nHit Enter to return to menu...");
+            Console.ReadLine();
+            Console.Clear();
+        } while (option != 0);
+    }
+
+    public static void DisplayMenu()
+    {
+        Console.WriteLine("------ Menu ------");
+        Console.WriteLine("1. Create a new Person");
+        Console.WriteLine("2. View all people");
+        Console.WriteLine("3. Remove a Person");
+        Console.WriteLine("5. Create and View Random SSN");
+        Console.WriteLine("0. Exit");
+        Console.WriteLine("------------------");
+    }
+
+    public static void MenuChoice(int option)
+    {
+        Random random = new Random();
+        switch (option)
+        {
+            case 1:
+                people.Add(new Person());
+                break;
+            case 2:
+                foreach (Person p in people)
+                {
+                    Console.WriteLine(p);
+                }
+                break;
+            case 3:
+                Console.WriteLine("Type the number of the person you would like to remove");
+                int personToRemove = Int32.Parse(Console.ReadLine());
+                people.Remove(people[personToRemove]);
+                break;
+            case 5:
+                Person rando = people[random.Next(people.Count())];
+                Console.WriteLine(rando.SSN);
+                break;
+            case 0:
+                Console.WriteLine("See ya!");
+                break;
+            default:
+                Console.WriteLine("Invalid option. Can you read?");
+                break;
+        }
+    }
+}
