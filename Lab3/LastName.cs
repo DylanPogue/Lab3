5c959bef80dd7cf5fb8066edaab0469f45869c6e
Dylan Pogue <POGUEDJ@ETSU.EDU>
2022-09-13 21:11:03 -0400

    lastname class

---
+++ Lab3/LastName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public enum LastName
+    {
+        Pogue, Silvers, Ransom, King, Daniels, Smith, Brooks, Hawley, Jones, Crowe
+    }
+}
