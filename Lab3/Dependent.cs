f07a2054f129f911e30325555dd36b83128dc8b5
Dylan Pogue <POGUEDJ@ETSU.EDU>
2022-09-13 21:30:35 -0400

    Dependent Class

---
+++ Lab3/Dependent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Dependent : Person
+    {
+        public Dependent()
+        {
+            Random rand = new Random();
+            DateTime dateToday = DateTime.Now;
+            int future = rand.Next(dateToday.Year, dateToday.Year + 10);
+            int month = rand.Next(1, 12);
+            int day = rand.Next(1, 31);
+
+            BirthDate = new DateTime(future, month, day);
+        }
+    }
+}
